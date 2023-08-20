e02774d705f7701e7ca0e6aa7777ff144639bd64
grumd <grumd0@gmail.com>
2023-08-20 11:43:23 +0000

    Deploying to gh-pages from @ grumd/pumpking@460cd9201b1972db044eae1a2540adf47cdd10fe 🚀

---
+++ assets/index-80549f17.js
@@ -0,0 +1,2 @@
+import{R as r,j as s}from"./index-b1e4bdb1.js";const e=r.memo(function({grade:t}){return!t||t==="?"?null:s.jsx("img",{src:`/grades/${t}.png`,alt:t})});export{e as G};
+//# sourceMappingURL=index-80549f17.js.map
