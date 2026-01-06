157582ece6a21712cc05c0aa34e98e739bcef2f4
grumd <grumd0@gmail.com>
2026-01-06 05:29:37 +0000

    Deploying to gh-pages from @ grumd/pumpking@8f3a443b04944a00ca6b26827adfef5236d778d5 🚀

---
+++ assets/useSingleChartQuery-CRMoInxx.js
@@ -0,0 +1,2 @@
+import{r as e,y as o,n}from"./index-DS6rzohy.js";import{u}from"./useFilter-DgSDiaFb.js";const a=({sharedChartId:r})=>{const s=u(),t=e.useMemo(()=>({scoring:s.scoring,sharedChartId:r}),[s.scoring,r]);return o(n.charts.chart.queryOptions(t))};export{a as u};
+//# sourceMappingURL=useSingleChartQuery-CRMoInxx.js.map
